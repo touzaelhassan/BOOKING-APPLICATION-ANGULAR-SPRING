098fb4b6ac9184bca784f6813b6b8cee3f0ec498
touzaelhassan <touzaelhassan@gmail.com>
2023-01-09 00:36:09 +0100

    Updating client, owner, admin and super admin authorities

--- BACKEND-APPLICATION/src/main/java/com/application/constants/AuthorityConstants.java
+++ BACKEND-APPLICATION/src/main/java/com/application/constants/AuthorityConstants.java
@@ -2,8 +2,8 @@ package com.application.constants;
 
 public class AuthorityConstants {
     public static final String[] USER_AUTHORITIES = { };
-    public static final String[] CLIENT_AUTHORITIES = { };
-    public static final String[] OWNER_AUTHORITIES = { "user:read", "user:update" };
-    public static final String[] ADMIN_AUTHORITIES = { "user:read", "user:create", "user:update" };
-    public static final String[] SUPER_ADMIN_AUTHORITIES = { "user:read", "user:create", "user:update", "user:delete" };
+    public static final String[] CLIENT_AUTHORITIES = { "reservation:create", "reservation:read", "reservation:update", "reservation:delete"};
+    public static final String[] OWNER_AUTHORITIES = {"user:read", "hotel:create", "hotel:read", "hotel:update", "hotel:delete", "room:create", "room:read", "room:update", "room:delete"};
+    public static final String[] ADMIN_AUTHORITIES = {"user:create", "user:read", "user:update"};
+    public static final String[] SUPER_ADMIN_AUTHORITIES = {"user:create", "user:read", "user:update", "user:delete"};
 }
