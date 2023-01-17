a4e4b37cb81946c17f2005c98c94e2baab487aad
touzaelhassan <touzaelhassan@gmail.com>
2023-01-17 10:57:10 +0100

    Adding room details modal to the home page

--- BACKEND-APPLICATION/src/main/java/com/application/Application.java
+++ BACKEND-APPLICATION/src/main/java/com/application/Application.java
@@ -38,7 +38,10 @@ public class Application {
     ){
 
         return args -> {
-          /*
+
+
+
+
             Admin admin = new Admin();
             admin.setUserId("534376");
             admin.setFirstname("El Hassan");
